893010347b756fe9bf31908a3fac73c694c822ac
WuLianN <610575024@qq.com>
2019-11-05 18:23:41 +0800

    搭建脚手架

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,45 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <div class="header"></div>
+    <div class="nav"></div>
+    <div class="footer"></div>
   </div>
 </template>
 
 <style lang="less">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@red: red;
+@green: green;
+@blue: blue;
+
+*{
+  margin: 0;
+  padding: 0;
 }
 
-#nav {
-  padding: 30px;
+.fullWidth {
+  width: 100vw;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+#app {
+  .fullWidth;
+  height: 100vh;
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.header {
+  .fullWidth;
+  height: 10vh;
+  background: @red;
+}
+
+.nav {
+  .fullWidth;
+  height: 80vh;
+  background: @green;
+}
+
+.footer {
+  .fullWidth;
+  height: 10vh;
+  background: @blue;
 }
 </style>
---
+++ vue.config.js
@@ -0,0 +1,43 @@
+module.exports = {
+    // 部署应用包时的基本 URL
+    publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
+  
+    /* 当运行 vue-cli-service build 时生成的生产环境构建文件的目录。
+       注意目标目录在构建之前会被清除 (构建时传入 --no-clean 可关闭该行为)。
+    */
+    outputDir: 'dist',
+  
+    // 放置生成的静态资源 (js、css、img、fonts) 的 (相对于 outputDir 的) 目录
+    assetsDir: '',
+  
+    // 指定生成的 index.html 的输出路径 (相对于 outputDir)。也可以是一个绝对路径。
+    indexPath: 'index.html',
+  
+    // 生成的静态资源在它们的文件名中包含了 hash 以便更好的控制缓存
+    filenameHashing: true,
+  
+    // eslint-loader 是否在保存的时候检查
+    lintOnSave: true,
+  
+    // 是否使用包含运行时编译器的Vue核心的构建
+    runtimeCompiler: false,
+  
+    devServer: {
+      open: true,
+      host: 'localhost',
+      port: 8080,
+      https: false,
+      hotOnly: false,
+      proxy: {
+        // 配置跨域
+        '/': {
+          target: 'http://localhost:3011',
+          ws: true,
+          changeOrigin: true,
+        },
+  
+      },
+      before: app => { }
+    }
+  }
+  
